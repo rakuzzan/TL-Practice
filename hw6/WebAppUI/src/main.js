16a3eec8ecc8a043d6c0682d90f87195dbc5627a
absolute <marishka1581@gmail.com>
2023-07-26 13:58:19 +0300

    доделал 6lw

--- hw6/WebAppUI/src/main.js
+++ hw6/WebAppUI/src/main.js
@@ -1,14 +1,125 @@
 import "./style.css";
 
-(async () => {
-  const response = await fetch("/api/users");
+const API_URL = "https://localhost:5140";
+const OFFSET_JSON = 2;
 
-  if (response.ok) {
-    const json = await response.json();
-    // eslint-disable-next-line no-console
-    console.log("json = ", json);
-  } else {
-    // eslint-disable-next-line no-console
-    console.log(`HTTP error: ${response.status}`);
-  }
-})();
+const getAllUsersBtn = document.querySelector("#btn-get-all");
+const getUserBtn = document.querySelector("#btn-get");
+const createUserBtn = document.querySelector("#btn-post");
+const deleteUserBtn = document.querySelector("#btn-delete");
+const updateUserBtn = document.querySelector("#btn-put");
+
+const getUserIdInput = document.querySelector("#input-get");
+const deleteUserIdInput = document.querySelector("#input-delete");
+const updateUserIdInput = document.querySelector("#input-put");
+
+const createUserInput = document.querySelector("#textarea-post");
+const updateUserInput = document.querySelector("#textarea-put");
+
+const getAllUsersResponseTextarea = document.querySelector("#textarea-response-get-all");
+const getUserResponseTextarea = document.querySelector("#textarea-response-get");
+const createUserResponseTextarea = document.querySelector("#textarea-response-post");
+const deleteUserResponseTextarea = document.querySelector("#textarea-response-delete");
+const updateUserResponseTextarea = document.querySelector("#textarea-response-put");
+
+const getAllUsers = async () => {
+  const response = await fetch(`${API_URL}/users`);
+
+  return response.json();
+};
+
+const getUserById = async id => {
+  const response = await fetch(`${API_URL}/users/${id}`);
+
+  return response.json();
+};
+
+const createUser = async user => {
+  const options = {
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  };
+
+  const response = await fetch(`${API_URL}/users`, options);
+
+  return response.json();
+};
+
+const updateUser = async (userId, updatedUser) => {
+  const options = {
+    body: JSON.stringify(updatedUser),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "PUT",
+  };
+
+  const response = await fetch(`${API_URL}/users/${userId}`, options);
+
+  return response.json();
+};
+
+const deleteUser = async id => {
+  const options = {
+    method: "DELETE",
+  };
+
+  const response = await fetch(`${API_URL}/users/${id}`, options);
+
+  return response.json();
+};
+
+const handleGetAllUsers = async () => {
+  const allUsers = await getAllUsers();
+
+  getAllUsersResponseTextarea.value = "";
+  getAllUsersResponseTextarea.value = JSON.stringify(allUsers, null, OFFSET_JSON);
+};
+
+const handleGetUser = async () => {
+  const userId = getUserIdInput.value;
+  const response = await getUserById(userId);
+
+  getUserResponseTextarea.value = "";
+  getUserResponseTextarea.value = JSON.stringify(response, null, OFFSET_JSON);
+};
+
+const handleCreateUser = async () => {
+  const newUser = createUserInput.value;
+  const validatedJSON = JSON.parse(newUser);
+
+  const response = await createUser(validatedJSON);
+
+  createUserResponseTextarea.value = "";
+  createUserResponseTextarea.value = JSON.stringify(response, null, OFFSET_JSON);
+};
+
+const handleDeleteUser = async () => {
+  const userId = deleteUserIdInput.value;
+
+  const response = await deleteUser(userId);
+
+  deleteUserResponseTextarea.value = "";
+  deleteUserResponseTextarea.value = JSON.stringify(response, null, OFFSET_JSON);
+};
+
+const handleUpdateUser = async () => {
+  const userId = updateUserIdInput.value;
+
+  const updatedUser = updateUserInput.value;
+  const validatedJSON = JSON.parse(updatedUser);
+
+  const response = await updateUser(userId, validatedJSON);
+
+  updateUserResponseTextarea.value = "";
+  updateUserResponseTextarea.value = JSON.stringify(response, null, OFFSET_JSON);
+};
+
+getAllUsersBtn.addEventListener("click", handleGetAllUsers);
+getUserBtn.addEventListener("click", handleGetUser);
+createUserBtn.addEventListener("click", handleCreateUser);
+deleteUserBtn.addEventListener("click", handleDeleteUser);
+updateUserBtn.addEventListener("click", handleUpdateUser);
